c571f3b1fd55b2f8f1ca9ffa097a3d8e9578460d
PHILLIP DULION <phillip.dulion@liveramp.com>
2021-11-07 19:25:00 -0800

    Turn off warning from reflectasm

--- src/main/java/com/dulion/greenhouse/GreenhouseApplication.java
+++ src/main/java/com/dulion/greenhouse/GreenhouseApplication.java
@@ -1,13 +1,16 @@
 package com.dulion.greenhouse;
 
+import com.esotericsoftware.reflectasm.MethodAccess;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GreenhouseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GreenhouseApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    Class.class.getModule().addOpens(
+        Class.class.getPackageName(),
+        MethodAccess.class.getModule());
+    SpringApplication.run(GreenhouseApplication.class, args);
+  }
 }
